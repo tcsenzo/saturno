b211ebe71c3a5b5e152014141f006e1b54af3402
Gustavo Melo <gcastromelo94@gmail.com>
2016-04-03 20:29:31 -0300

    estrutura do header e menu lateral

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,1 +1,13 @@
-var x ='lalalala';
+(function() {
+  var menuToggle = document.querySelector('.menu-toggle'),
+      sideMenu = document.querySelector('.side-menu'),
+      closeSideMenu = document.querySelector('.side-menu .close');
+
+  menuToggle.addEventListener('click', function() {
+    sideMenu.style.left = 0;
+  });
+
+  closeSideMenu.addEventListener('click', function() {
+    sideMenu.style.left = '-30%';
+  });
+}())

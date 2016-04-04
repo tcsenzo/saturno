3b53307ad16aaffe080b8f0f917fd0508916f1be
Gustavo Melo <gcastromelo94@gmail.com>
2016-04-03 21:06:22 -0300

    iniciando container principal

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,13 +1,16 @@
 (function() {
   var menuToggle = document.querySelector('.menu-toggle'),
       sideMenu = document.querySelector('.side-menu'),
-      closeSideMenu = document.querySelector('.side-menu .close');
+      closeSideMenu = document.querySelector('.side-menu .close'),
+      pageContainer = document.querySelector('.page-container');
 
   menuToggle.addEventListener('click', function() {
     sideMenu.style.left = 0;
+    pageContainer.style.paddingLeft = '100px';
   });
 
   closeSideMenu.addEventListener('click', function() {
-    sideMenu.style.left = '-32%';
+    sideMenu.style.left = '-310px';
+    pageContainer.style.paddingLeft = 0;
   });
 }())

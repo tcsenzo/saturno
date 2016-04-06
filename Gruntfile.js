63194aa2daa1318c0cea4d4da4b023433d1d3a73
Gustavo Melo <gcastromelo94@gmail.com>
2016-04-06 00:41:30 +0000

    separando tasks do grunt em arquivos

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,6 @@
 'use strict';
 module.exports = function(grunt) {
   require('load-grunt-config')(grunt);
-  // todo: separar essas configs para arquivos externos
-  grunt.initConfig({
-    babel: {
-      options: {
-        sourceMap: true,
-        presets: ['es2015']
-      },
-      dist: {
-        files: [{
-          "expand": true,
-          "cwd": "app/",
-          "src": ["**/*.js", "!assets/**/*"],
-          "dest": "dist/",
-          "ext": ".js"
-        }]
-      }
-    },
-
-    browserify: {
-      dist: {
-        options: {
-          "transform": [ ["babelify", { "presets": ["es2015"] }] ]
-        },
-        files: [{
-          "expand": true,
-          "cwd": "app/assets/js/",
-          "src": ["**/*.js"],
-          "dest": "dist/assets/js/",
-          "ext": ".js"
-        }]
-      }
-    },
-
-    watch: {
-      es6Backend: {
-        files: ['app/**/*.js', '!app/assets/**/*'],
-        tasks: ['newer:babel']
-      },
-      es6Frontend: {
-        files: ['app/assets/js/**/*.js'],
-        tasks: ['newer:browserify']
-      },
-      stylus: {
-        files: ['app/assets/stylus/**/*.styl'],
-        tasks: ['newer:stylus']
-      }
-    },
-  });
 
   grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-browserify');
--- grunt/stylus.js
+++ grunt/stylus.js
@@ -1,23 +1,23 @@
 module.exports = {
     compile: {
-        options: {
-          use: [
-            require('jeet'),
-            require('rupture')
-          ],
-          paths: [
-            "node_modules/jeet/stylus",
-            "node_modules/rupture"
-          ]
-        },
-        files: [
-          {
-            'expand': true,
-            'cwd': 'app/assets/stylus/pages/',
-            'src': '**/page.styl',
-            'dest': 'dist/assets/css/pages/',
-            'ext': '.css'
-          }
+      options: {
+        use: [
+          require('jeet'),
+          require('rupture')
+        ],
+        paths: [
+          "node_modules/jeet/stylus",
+          "node_modules/rupture"
         ]
+      },
+      files: [
+        {
+          'expand': true,
+          'cwd': 'app/assets/stylus/pages/',
+          'src': '**/page.styl',
+          'dest': 'dist/assets/css/pages/',
+          'ext': '.css'
+        }
+      ]
     }
 }

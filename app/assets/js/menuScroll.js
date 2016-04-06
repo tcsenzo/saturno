ec56683454c910059649d9ed4220d234cda52f7b
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-04-06 01:14:17 -0300

    menu fixo no scroll

--- app/assets/js/menuScroll.js
+++ app/assets/js/menuScroll.js
@@ -1,16 +1,24 @@
 export default class MenuScroll {
     constructor() {
         this.lastScrollTop = 0;
+        this.$header = $(`.header`);
     }
     bindScroll(e) {
         let that = e.data.that;
-        that.getScrollDirection();
+        if ($(window).scrollTop() <= 60) {
+          //that.$header.removeClass(`fixed`);
+          that.$header.css({top: 0});
+        }
+        else if ($(this).scrollTop() > 60 && that.getScrollDirection() === `up`) {
+          //that.$header.addClass(`fixed`);
+          that.$header.css({top: $(window).scrollTop()});
+        }
     }
-    
+
     getScrollDirection() {
         let scrollTop = $(window).scrollTop(),
-            scrollDirection = scrollTop > this.lastScrollTop ? 'down' : 'up';
-            
+            scrollDirection = scrollTop > this.lastScrollTop ? `down` : `up`;
+
         this.lastScrollTop = scrollTop;
         return scrollDirection;
     }

db7e08ed92891b2bfc2b51d8f70575d2375c6a7d
Gustavo Melo <gcastromelo94@gmail.com>
2016-07-09 12:20:35 -0300

    correcoes no botao e nos links/menus

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,14 +1,10 @@
 module.exports = {
-  es6Backend: {
-    files: ['app/**/*.js', '!app/assets/**/*'],
-    tasks: ['newer:babel']
-  },
   es6Frontend: {
     files: ['app/assets/js/**/*.js'],
     tasks: ['newer:browserify']
   },
   stylus: {
     files: ['app/assets/stylus/**/*.styl'],
-    tasks: ['newer:stylus']
+    tasks: ['stylus']
   }
 }

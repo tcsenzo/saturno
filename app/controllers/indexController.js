23fb7b30b960029e7f6daae9953e491ab376fdde
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-07-21 23:51:37 -0300

    3 requests asyncs pra listar os 3 filtros diferentes

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,11 +1,37 @@
-let services = require(`../services`);
+let services = require(`../services`),
+    _ = require(`underscore`);
 
 class Index {
-  static index(res) {
+
+  index(res) {
+    let filters = {},
+        finished = {},
+        that = this;
+
+    finished = _.after(3, () => {
+      that.doRender(res, filters);
+    });
+
     services.events.all((error, response, body) => {
-      res.render('index/index', JSON.parse(body));
+      filters.fiveHours = JSON.parse(body).events;
+      finished();
+    });
+
+    services.events.all((error, response, body) => {
+      filters.eightHours = JSON.parse(body).events;
+      finished();
+    });
+
+    services.events.all((error, response, body) => {
+      filters.twelveHours = JSON.parse(body).events;
+      finished();
     });
   }
+
+  doRender(res, filters) {
+    res.render("index/index", { "filters": filters });
+  }
+
 }
 
-module.exports = Index;
+module.exports = new Index();

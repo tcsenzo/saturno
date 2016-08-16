77254a01a754f4a62ceb2b6fae2c2228071f9cf0
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-08-15 21:14:54 -0300

    remove debug

--- app/services/login.js
+++ app/services/login.js
@@ -7,30 +7,6 @@ class Login {
       cb(error, response, body);
     });
   }
-
-  static teste(cookie, cb) {
-    let jsonTemplate = {
-      "name": "Teatro NET SP",
-      "address": {
-          "street": "Rua Olimpíadas",
-          "number": "360",
-          "district": "Vila Olimpia",
-          "city": "São Paulo",
-          "state": "SP",
-          "country": "Brasil",
-          "zip_code": "04551-000"
-      }
-    }
-
-    request.post({
-        url: `${config.theaterEventsApi}/theaters`, json: jsonTemplate,
-        headers: {
-          'cookie': cookie
-        }
-      }, (error, response, body) => {
-      cb(error, response, body);
-    });
-  }
 }
 
 module.exports = Login;

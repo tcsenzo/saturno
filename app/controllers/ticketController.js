f2c14f793a0887c414f849f38c413648db08783d
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-08-26 20:53:06 -0300

    remove comentarios

--- app/controllers/ticketController.js
+++ app/controllers/ticketController.js
@@ -7,14 +7,6 @@ class TicketController {
   show(req, res) {
     let ticketsJSON = JSON.parse(decodeURIComponent(req.query.showTicketsJSON));
     res.render('ticket/show', {ticket: ticketsJSON});
-    // helpers.requestMid.request({
-    //   req: req,
-    //   res: res,
-    //   url: `${config.checkoutApi}/tickets/${req.params.hash}`,
-    //   cb: (apiError, apiRes, apiBody) => {
-    //     res.render('ticket/show', {ticket: ticketJSON});
-    //   }
-    // });
   }
 }
 

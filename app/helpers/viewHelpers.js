4d7c1ada46c7b1e53a7ad0a236bae177a228f3ed
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-08-27 13:31:16 -0300

    inclui um segundo script na pagina caso a pagina tenha um script custom, senao, apenas mantem o main.js

--- app/helpers/viewHelpers.js
+++ app/helpers/viewHelpers.js
@@ -1,6 +1,13 @@
+let fs = require('fs');
+
 class ViewHelpers {
+
   currentFileName() {
     return __dirname;
+  }
+
+  assetExists(path) {
+    return fs.existsSync(`${__dirname}/../assets/${path}`);
   }
 }
 

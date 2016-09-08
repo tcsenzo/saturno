ce234129bede1b09d1e3d5b6ae5578da3d21c877
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-08 18:56:16 -0300

    detalhes de layout no side menu

--- app/assets/js/sideMenu.js
+++ app/assets/js/sideMenu.js
@@ -9,7 +9,7 @@ SideMenu.prototype.binds = function() {
   var that = this;
 
   $(`.menu-toggle`).on(`click`, {that: this}, this.onOpenMenu);
-  $('.side-menu .close').on('click', {that: this}, this.onCloseMenu);
+  $('.side-menu .close-side-menu').on('click', {that: this}, this.onCloseMenu);
 };
 
 SideMenu.prototype.onOpenMenu = function (e) {

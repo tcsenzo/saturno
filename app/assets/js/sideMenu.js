1beba0b6e26a263731fbb8263972f96ef4c23287
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-13 19:38:41 -0300

    eventos de esconder o menu no click do overlay e no keyup do esc

--- app/assets/js/sideMenu.js
+++ app/assets/js/sideMenu.js
@@ -8,8 +8,9 @@ function SideMenu() {
 SideMenu.prototype.binds = function() {
   var that = this;
 
-  $(`.menu-toggle`).on(`click`, {that: this}, this.onOpenMenu);
-  $('.side-menu .close-side-menu').on('click', {that: this}, this.onCloseMenu);
+  $('.menu-toggle').on('click', {that: this}, this.onOpenMenu);
+  $('.side-menu .close-side-menu, .overlay').on('click', {that: this}, this.onCloseMenu);
+  $(document).on('keyup', {that: this}, this.onKeyup);
 };
 
 SideMenu.prototype.onOpenMenu = function (e) {
@@ -24,6 +25,14 @@ SideMenu.prototype.onCloseMenu = function (e) {
 
   that.helpers.hideSideMenu();
   that.helpers.hideOverlay();
+};
+
+SideMenu.prototype.onKeyup = function (e) {
+  var that = e.data.that;
+
+  if (e.keyCode === 27) {
+    that.onCloseMenu(e);
+  }
 };
 
 module.exports = SideMenu;

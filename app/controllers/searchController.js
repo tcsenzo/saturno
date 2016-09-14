5a5282cec38d8e8a2b2499b118b118dc6cc343ee
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-14 01:05:18 -0300

    implementa a busca pela api sem layout

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -1,6 +1,18 @@
+let helpers = require('../helpers'),
+    config = require('../config');
+
 class SearchController {
   index(req, res) {
-    res.render('search/index');
+    helpers.requestMid.request({
+      req: req,
+      res: res,
+      url: `${config.theaterEventsApi}/events?q=${req.query.q}`,
+      cb: (apiError, apiRes, apiBody) => {
+        if(apiRes.statusCode === 200) {
+          res.render('search/index', {'events': JSON.parse(apiBody).events});
+        }
+      }
+    });
   }
 }
 

b397bb8651b27601472660ac670bcd3d943b287d
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-22 20:00:30 -0300

    remove metodo de teste

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -16,40 +16,6 @@ class EventController {
       }
     });
   }
-
-  create(req, res) {
-    let jsonTemplate = {
-      "name": "GUAIANAZESSSS",
-      "description": "Um evento para toda a familia",
-      "price": "22.0",
-      "available_quantity": 20,
-      "scheduled_date": "2017-12-03T10:15:30",
-      "theater": {
-        "name": "Teatro NET SP",
-        "address": {
-            "street": "Rua Olimpíadas",
-            "number": "360",
-            "district": "Vila Olimpia",
-            "city": "São Paulo",
-            "state": "SP",
-            "country": "Brasil",
-            "zip_code": "04551-000"
-        }
-      }
-    };
-
-
-    helpers.requestMid.request({
-      req: req,
-      res: res,
-      method: 'POST',
-      url: `${config.theaterEventsApi}/events/`,
-      jsonParams: jsonTemplate,
-      cb: (apiError, apiRes, apiBody) => {
-        console.log('====================== ACHO QUE FOI =========================')
-      }
-    });
-  }
 }
 
 module.exports = new EventController();

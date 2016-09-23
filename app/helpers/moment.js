65d4f0e9d31e70bbaf7985095f19021d58676222
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-23 10:34:25 -0300

    bugfix na conversão de datas

--- app/helpers/moment.js
+++ app/helpers/moment.js
@@ -4,15 +4,14 @@ class Moment {
 
   constructor() {
     this.humanFormat = 'DD/MM/YYYY HH:mm';
-    this.systemFormat = 'YYYY-MM-DDTHH:mm:ss';
   }
 
   systemToHuman(dateString) {
-    return moment(dateString, this.systemFormat).format(this.humanFormat);
+    return moment(dateString).format(this.humanFormat);
   }
 
   humanToSystem(dateString) {
-    return moment(dateString, this.humanFormat).format(this.systemFormat) + 'Z';
+    return moment(dateString, this.humanFormat).utc().format();
   }
 }
 
